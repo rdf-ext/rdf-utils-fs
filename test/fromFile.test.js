494e0795830bf51e4eb3821ad6d1dd57f4a50626
Cristian <cristianvasquez@gmail.com>
2022-04-21 19:53:12 +0200

    chore: Add test for file not found

--- test/fromFile.test.js
+++ test/fromFile.test.js
@@ -1,5 +1,7 @@
-const { strictEqual, throws } = require('assert')
+const { strictEqual } = require('assert')
 const { resolve } = require('path')
+const assertThrows = require('assert-throws-async')
+const getStream = require('get-stream')
 const { isReadable } = require('isstream')
 const { describe, it } = require('mocha')
 const rdf = require('rdf-ext')
@@ -9,32 +11,36 @@ const example = require('./support/example')
 describe('fromFile', () => {
   it('should create a quad stream', async () => {
     const stream = fromFile(resolve(__dirname, 'support/example.nt'))
-
     stream.resume()
-
     strictEqual(isReadable(stream), true)
   })
 
   it('should forward options to parser', async () => {
     const stream = fromFile(resolve(__dirname, 'support/example.ttl'), { baseIRI: 'http://example.org/' })
     const dataset = await rdf.dataset().import(stream)
-
     strictEqual(dataset.toCanonical(), example().toCanonical())
   })
 
-  it('should throw an error if the file extension is unknown', () => {
-    throws(() => {
+  it('should throw an error if the file extension is unknown', async () => {
+    await assertThrows(async () => {
       fromFile('test.jpg')
-    })
+    }, Error, /Unknown file extension/)
   })
 
-  it('should throw an error if the media type is unknown', () => {
-    throws(() => {
+  it('should throw an error if the media type is unknown', async () => {
+    await assertThrows(async () => {
       fromFile('test.jpg', {
         extensions: {
           jpg: 'image/jpeg'
         }
       })
-    })
+    }, Error, /No parser available for media type/)
+  })
+
+  it('should throw an error if the file does not exist', async () => {
+    await assertThrows(async () => {
+      const stream = fromFile('void.ttl')
+      await getStream.array(stream)
+    }, Error, /ENOENT: no such file or directory/)
   })
 })
